f96b1d421f044637bd91611aa96c19ed5d4d6c23
Bhavesh094 <164846828+Bhavesh094@users.noreply.github.com>
2024-10-10 12:44:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import streamlit as st
 import numpy as np
-import cv2
+from opencv import cv2
 from tensorflow.keras.models import load_model
 from PIL import Image
 import time
